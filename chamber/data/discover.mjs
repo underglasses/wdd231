21c281a0c798740998606c43e669e3da0b804b6b
underglasses <chingamarcos@gmail.com>
2026-08-07 11:09:37 -0500

    update css file images

--- chamber/data/discover.mjs
+++ chamber/data/discover.mjs
@@ -3,47 +3,47 @@ export const places = [
     "name": "Pacoche",
     "address": "Pacoche, Manta, Ecuador",
     "description": "El Refugio de Vida Silvestre Marino Costero Pacoche es un área protegida que combina bosques húmedos y senderos turísticos donde se pueden observar monos aulladores, aves y flora tropical. Es un destino ideal para ecoturismo y conservación, ofreciendo experiencias únicas en contacto directo con la naturaleza.",
-    "image": "wdd231/chamber/images/pacoche.webp"
+    "image": "pacoche.webp"
   },
   {
     "name": "San Lorenzo",
     "address": "San Lorenzo, Manta, Ecuador",
     "description": "El Faro de San Lorenzo se alza sobre un acantilado rocoso frente al océano Pacífico. Es un punto emblemático para visitantes que buscan vistas panorámicas del mar, formaciones rocosas y atardeceres inolvidables. Su entorno natural lo convierte en un atractivo turístico de gran valor paisajístico.",
-    "image": "wdd231/chamber/images/san_lorenzo.webp"
+    "image": "san_lorenzo.webp"
   },
   {
     "name": "Manta Nightlife",
     "address": "Centro de Manta, Ecuador",
     "description": "La vida nocturna de Manta ofrece una mezcla vibrante de restaurantes, bares y discotecas. Los visitantes disfrutan de música en vivo, gastronomía local y un ambiente festivo que refleja la energía de la ciudad. Es un espacio ideal para compartir con amigos y experimentar la cultura urbana.",
-    "image": "wdd231/chamber/images/centro.webp"
+    "image": "centro.webp"
   },
   {
     "name": "Monumento al Pescador",
     "address": "Av. Malecón, Manta, Ecuador",
     "description": "El Monumento al Pescador honra la tradición pesquera de Manta. Con su diseño moderno y colorido, se ha convertido en un símbolo cultural y turístico. Los visitantes pueden recorrer la plaza, tomar fotografías y aprender sobre la importancia histórica de la pesca en la economía local.",
-    "image": "wdd231/chamber/images/playa_sur.webp"
+    "image": "playa_sur.webp"
   },
   {
     "name": "La Calle de los Murales",
     "address": "Centro Histórico, Manta, Ecuador",
     "description": "La Calle de los Murales es un espacio cultural donde el arte urbano cobra vida. Sus coloridas fachadas y esculturas invitan a recorrer un ambiente lleno de creatividad. Es un lugar perfecto para apreciar la identidad local y disfrutar de un paseo tranquilo entre obras artísticas.",
-    "image": "wdd231/chamber/images/plaza_egas.webp"
+    "image": "plaza_egas.webp"
   },
   {
     "name": "Plaza Norte",
     "address": "Via San Mateo, Manta, Ecuador",
     "description": "El Refugio de Vida Silvestre Marino Costero Pacoche es un área protegida que combina bosques húmedos y senderos turísticos donde se pueden observar monos aulladores, aves y flora tropical. Es un destino ideal para ecoturismo y conservación, ofreciendo experiencias únicas en contacto directo con la naturaleza.",
-    "image": "wdd231/chamber/images/plaza_norte.webp"
+    "image": "plaza_norte.webp"
   },{
     "name": "Plaza sur",
     "address": "Av Cultura, Manta, Ecuador",
     "description": "El Refugio de Vida Silvestre Marino Costero Pacoche es un área protegida que combina bosques húmedos y senderos turísticos donde se pueden observar monos aulladores, aves y flora tropical. Es un destino ideal para ecoturismo y conservación, ofreciendo experiencias únicas en contacto directo con la naturaleza.",
-    "image": "wdd231/chamber/images/plaza_sur.webp"
+    "image": "plaza_sur.webp"
   },
   {
     "name": "Plaza Mar",
     "address": "Av. Flavio Reyes, Manta, Ecuador",
     "description": "El Edificio Moderno es un ejemplo de arquitectura contemporánea en Manta. Su diseño innovador y funcionalidad lo convierten en un punto de referencia para la ciudad. Los visitantes pueden admirar su estructura, aprender sobre su historia y disfrutar de la vista panorámica desde sus niveles superiores.",
-    "image": "wdd231/chamber/images/plaza_mar.webp"
+    "image": "plaza_mar.webp"
   }
 ];
